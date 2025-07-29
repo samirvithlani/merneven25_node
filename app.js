7f6f74a23fa9ec9ef3b04331531f7bfcbf409581
samirvithlani <samir.vithlani83955@gmail.com>
2025-07-29 18:37:16 +0530

    get api

--- app.js
+++ app.js
@@ -1,18 +1,64 @@
-console.log("App file loaded successfully!");
-// const username = require("./user") //user file load...
-// console.log(`username = ${username}`)
+const express = require("express")
+//expres object..
+const app = express() //
 
-const user = require("./user")
-console.log(user)
-console.log(`${user.username}`)
-console.log(`${user.userAge}`)
 
-// const employee = require("./employee") //employee == function
-// //console.log(employee)
-// employee("ram",22,"ram@gmail")
+//url
+//localhost:3000/test
+app.get("/test",(req,res)=>{
 
-const employee = require("./employee")
-//console.log(employee)
-employee.setEmployeeData("ram",22,"ram@gmail")
-var data = employee.getEmployee()
-console.log(data)
+    console.log("test api called....")
+    res.send("test api hit")
+})
+
+const user = {
+    id:101,
+    name:"amit",
+    age:23,
+    status:"active"
+}
+
+app.get("/user",(req,res)=>{
+
+    //res.json(user)
+    res.json({message:"user fetch successfully!!",data:user})
+})
+
+const users = [
+    {
+        id:1,
+        name:"ram",
+        age:22,
+        status:"active",
+    },
+    {
+        id:2,
+        name:"seeta",
+        age:22,
+        status:"not active",
+    },
+    {
+        id:3,
+        name:"ramila",
+        age:24,
+        status:"active",
+    }
+]
+
+//localhost:3000/users
+app.get("/users",(req,res)=>{
+    res.json({
+        message:"users fetched..",
+        users:users
+    })
+})
+
+
+
+
+
+//server..
+const PORT = 3000
+app.listen(PORT,()=>{
+    console.log(`server started on port ${PORT}`)
+})
