ece14e981511cedff6b68be990362f2912625746
samirvithlani <samir.vithlani83955@gmail.com>
2025-08-26 18:22:37 +0530

    delete update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,54 +1,92 @@
-const userModel = require("../models/UserModel")
+const userModel = require("../models/UserModel");
 
 //function
 
-const getAllUsers = async(req,res)=>{
-
-    const users = await userModel.find() //[]
-    if(users && users.length>0){
-        res.json({
-            message:"user fateched successfully!!",
-            data:users
-        })
-    }
-    else{
-        res.json({
-            message:"user not found.."
-        })
-    }
-
-}
-
-const getUserById = async(req,res)=>{
-
-    const id = req.params.id
-    const foundUser = await userModel.findById(id) //{}
-    if(foundUser){
-        res.json({
-            message:"user found",
-            data:foundUser
-        })
-    }
-    else{
-        res.json({
-            message:"user not found.."
-        })
-    }
-
-}
-
-const addUser=async(req,res)=>{
-    //db.users.create(req.body)
-    //userModel.create(req.body)
-    const savedUser = await userModel.create(req.body)
+const getAllUsers = async (req, res) => {
+  const users = await userModel.find(); //[]
+  if (users && users.length > 0) {
     res.json({
-        message:"user saved successfully !!",
-        data:savedUser
-    })
+      message: "user fateched successfully!!",
+      data: users,
+    });
+  } else {
+    res.json({
+      message: "user not found..",
+    });
+  }
+};
+
+const getUserById = async (req, res) => {
+  const id = req.params.id;
+  const foundUser = await userModel.findById(id); //{}
+  if (foundUser) {
+    res.json({
+      message: "user found",
+      data: foundUser,
+    });
+  } else {
+    res.json({
+      message: "user not found..",
+    });
+  }
+};
+
+const addUser = async (req, res) => {
+  //db.users.create(req.body)
+  //userModel.create(req.body)
+  const savedUser = await userModel.create(req.body);
+  res.json({
+    message: "user saved successfully !!",
+    data: savedUser,
+  });
+};
+
+const deleteUser = async (req, res) => {
+  //id ==>req.params.id
+  //db.users.remove({id:101})
+  const id = req.params.id;
+  const deletedUser = await userModel.findByIdAndDelete(id);
+
+  if (deletedUser) {
+    res.json({
+      message: "user deleted successfully !!",
+      data: deletedUser,
+    });
+  } else {
+    res.json({
+      message: "user not found",
+      data: null,
+    });
+  }
+};
+
+const updateUser= async(req,res)=>{
+
+    //update table name set ,, ? where id =?
+    //db.users.update({set},{id})
+    const id = req.params.id;
+    //req.body
+    //const updatedUser = await userModel.findByIdAndUpdate(id,req.body) //old data
+    const updatedUser = await userModel.findByIdAndUpdate(id,req.body,{new:true}) //new data
+    if(updatedUser){
+        res.json({
+            message:"user updated successfully !!",
+            data:updatedUser
+        })
+    }
+    else{
+        res.json({
+            message:"user not found !!",
+            data:null
+        })
+    }
+
 }
 
 module.exports = {
-    getAllUsers,
-    getUserById,
-    addUser
-}
+  getAllUsers,
+  getUserById,
+  addUser,
+  deleteUser,
+  updateUser
+};
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -3,4 +3,6 @@ const userController = require("../controllers/UserController")
 router.get("/users",userController.getAllUsers)
 router.get("/user/:id",userController.getUserById)
 router.post("/user",userController.addUser)
+router.delete("/user/:id",userController.deleteUser)
+router.put("/user/:id",userController.updateUser)
 module.exports = router
