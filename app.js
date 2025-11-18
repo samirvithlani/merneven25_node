afb6dfd0fbbcded04070621327d523a549322983
samirvithlani <samir.vithlani83955@gmail.com>
2025-11-18 18:26:25 +0530

    node cron

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require('cors')
+const cron = require("node-cron")
 //expres object..
 const app = express() //
 app.use(express.json()) //application/json -- type
+app.use(cors())
 
 //routes
 const userRoutes = require("./routes/UserRoutes")
@@ -24,6 +27,33 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern_evn_2_node").then(()=>{
 console.log("db connected..")
 }).catch((err)=>{
 
+})
+
+//cron jobs
+ 
+// *    *    *   *      *
+// sec  min hour month weekday
+
+//* every time
+//, multipule values
+//- range
+// / steps
+
+//every 10 second job.
+
+//*/10 * * * *
+
+//every min
+// * * * * *
+
+//runs every day 7 am
+
+//0 0 7 * *
+
+
+
+cron.schedule("*/10 * * * * *",()=>{
+    console.log("job runs at every 10 seconds")
 })
 
 
